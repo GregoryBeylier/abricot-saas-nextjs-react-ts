58b1362c95d620457618e2196869240dee9eaae7
Gregory <samsung261101@gmail.com>
2026-06-18 01:28:08 +0200

    feat: components liste and css dashboard

--- abricot_front_end/app/(app)/dashboard/page.tsx
+++ abricot_front_end/app/(app)/dashboard/page.tsx
@@ -5,6 +5,9 @@ import { useState } from 'react'
 import { fetchProfile } from '@/lib/api'
 import VueListe from '@/components/dashboard/VueListe'
 import VueKanban from '@/components/dashboard/VueKanban'
+import iconListe from '@/public/liste.svg'
+import iconKanban from '@/public/kanban.svg'
+import Image from 'next/image'
 
 // interface pour typer la réponse de l'api
 interface UserProfile {
@@ -30,19 +33,37 @@ export default function DashboardPage() {
   const [vue, setVue] = useState('liste')
   console.log(vue)
   return (
-    <div className="bg-[#F9FAFB] min-h-screen p-8">
-      <div className="flex justify-between">
-        <div>
-          <h1>Tableau de bord</h1>
-          <p>Bonjour {name}, voici un aperçu de vos projets et tâche</p>
+    <div className="bg-[#F9FAFB] px-[112px] pt-[40px] pb-[50px]">
+      <div className="flex justify-between items-center mb-6">
+        <div className="mt-[89px]">
+          <h1 className="text-2xl font-semibold">Tableau de bord</h1>
+          <p>Bonjour {name}, voici un aperçu de vos projets et tâches</p>
         </div>
-        <Button>+ créer un projet </Button>
+        <Button className="h-[50px] px-[30px] rounded-[10px] text-base font-normal mt-[89px]">
+          + Créer un projet
+        </Button>
       </div>
-      <div>
-        <button onClick={() => setVue('liste')}>Liste</button>
-        <button onClick={() => setVue('kanban')}>Kanban</button>
-        {vue === 'liste' ? <VueListe /> : <VueKanban />}
+      <div className="flex gap-4 mb-6 mt-[60px]">
+        <button
+          onClick={() => setVue('liste')}
+          className={`flex items-center gap-2 px-4 py-2 rounded-lg text-[var(--color-abricot)] ${
+            vue === 'liste' ? 'bg-[#FFE8D9]' : 'bg-white'
+          }`}
+        >
+          <Image src={iconListe} alt="" width={20} height={20} />
+          Liste
+        </button>
+        <button
+          onClick={() => setVue('kanban')}
+          className={`flex items-center gap-2 px-4 py-2 rounded-lg text-[var(--color-abricot)] ${
+            vue === 'kanban' ? 'bg-[#FFE8D9]' : 'bg-white'
+          }`}
+        >
+          <Image src={iconKanban} alt="" width={20} height={20} />
+          Kanban
+        </button>
       </div>
+      {vue === 'liste' ? <VueListe /> : <VueKanban />}
     </div>
   )
 }
--- abricot_front_end/components/dashboard/VueListe.tsx
+++ abricot_front_end/components/dashboard/VueListe.tsx
@@ -1,3 +1,119 @@
+'use client'
+import { fetchAssignedTasks } from '@/lib/api'
+import { useQuery } from '@tanstack/react-query'
+import { format } from 'date-fns'
+import { fr } from 'date-fns/locale'
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import { Input } from '@/components/ui/input'
+import { Search, Folder, Calendar, MessageSquare } from 'lucide-react'
+
+interface Task {
+  id: string
+  title: string
+  description: string
+  status: string
+  dueDate: string | null
+  comments: { id: string }[]
+}
+
+interface AssignedTasksResponse {
+  data: {
+    tasks: Task[]
+  }
+}
+
 export default function VueListe() {
-  return <h1>Vue liste</h1>
+  // Utilisation de la hook useQuery pour récupérer les tâche
+  const { data } = useQuery<AssignedTasksResponse>({
+    queryKey: ['tasks'],
+    queryFn: fetchAssignedTasks,
+  })
+
+  // récupération des tâches
+  const tasks = data?.data?.tasks
+
+  // mapping pour les états des tâches
+  const statusLabels = {
+    TODO: 'À faire',
+    IN_PROGRESS: 'En cours',
+    DONE: 'Terminée',
+  }
+
+  // mapping pour les couleurs des états des tâches
+  const statusColors = {
+    TODO: 'bg-red-100 text-red-400',
+    IN_PROGRESS: 'bg-orange-100 text-orange-400',
+    DONE: 'bg-green-100 text-green-400',
+  }
+
+  // hook pour la recherche des tâches
+  const [search, setSearch] = useState('')
+  const tasksFiltrees = tasks?.filter((task) =>
+    task.title.toLowerCase().includes(search.toLowerCase())
+  )
+
+  return (
+    <div className="flex flex-col w-full bg-white rounded-lg p-6 gap-4 pt-[41px] px-[59px] ">
+      <div className="flex justify-between items-center mb-[30px]">
+        <div>
+          <h1 className="font-semibold">Mes tâches assignées</h1>
+          <p className="text-sm text-gray-500">Par ordre de priorité</p>
+        </div>
+        <div className="relative">
+          <Input
+            placeholder="Rechercher une tâche"
+            className="w-[357px] h-[53px] rounded-[8px] border px-[32px] py-[23px] bg-white"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <Search
+            className="absolute right-[32px] top-1/2 -translate-y-1/2 text-gray-400"
+            size={20}
+          />
+        </div>
+      </div>
+      {tasksFiltrees?.map((task) => (
+        <div
+          className="bg-white rounded-lg border p-4 flex justify-between h-[162px]"
+          key={task.id}
+        >
+          <div className="flex flex-col justify-between py-2">
+            <div>
+              <h2 className="font-medium">{task.title}</h2>
+              <p className="text-gray-500 text-sm">{task.description}</p>
+            </div>
+            <div className="flex items-center gap-2 text-sm text-gray-500">
+              <Folder size={16} className="text-gray-400" fill="currentColor" />
+              <span>Nom du projet</span>
+              <span>|</span>
+              <Calendar size={16} className="text-gray-400" />
+              <span>
+                {task.dueDate
+                  ? format(new Date(task.dueDate), 'd MMM', { locale: fr })
+                  : ''}
+              </span>
+              <span>|</span>
+              <MessageSquare
+                size={16}
+                className="text-gray-400"
+                fill="currentColor"
+              />
+              <span>{task.comments.length}</span>
+            </div>
+          </div>
+          <div className="flex flex-col items-end justify-between pr-4 pb-2">
+            <span
+              className={`mt-2 rounded-full px-[16px] py-[4px] text-xs h-[25px] flex items-center ${statusColors[task.status as keyof typeof statusColors]}`}
+            >
+              {statusLabels[task.status as keyof typeof statusLabels]}
+            </span>
+            <Button className="h-[40px] rounded-[10px] px-[32px] bg-[#1F1F1F] text-white">
+              Voir
+            </Button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
